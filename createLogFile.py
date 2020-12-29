2fe90d752d8d132b62ecade6679f96cd95bd28d7
Anurag Patil <49953149+Anu1996rag@users.noreply.github.com>
2020-12-29 10:49:29 +0530

    Delete createLogFile.py

--- createLogFile.py
+++
@@ -1,7 +0,0 @@
-import logging.config, os
-
-
-def createLogFile(logfilename):
-    return logging.config.fileConfig(fname='loggingConfiguration.conf', defaults={'logfilename': logfilename})
-
-# createLogFile('abc')
